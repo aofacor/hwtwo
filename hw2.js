f7ae3c05fd0770f3f510badb6a9e8098ef359507
Alejandro O'Farrill <ofas@dhcp-10-105-140-119.wireless.northwestern.private>
2018-02-13 15:05:44 -0600

    review changes in class

--- hw2.js
+++ hw2.js
@@ -24,15 +24,21 @@ let blockchain = [
 //every function in javascript is defined with function(){}
 //we can call the parameter name whatever we want. This is another variable we are defining on the fly. What ever we call it is completely up to us.
 
-let getBalance = function(username){
-
-  
+let getBalance = function(user) {
+  let runningTotal = 0;
+  for (i=0; i<blockchain.length; i++) {
+    let transaction = blockchain[i];
+    if (transaction.fromUser === user) {
+      runningTotal = runningTotal - transaction.amount;
+      }
+      if (transaction.toUser === user) {
+        runningTotal = runningTotal + transaction.amount;
+      }
+  }
+return runningTotal;
 }
-
-
 
 //getBalance is a function
 console.log("Brian's KelloggCoin bal is " + getBalance("brian"));
 console.log("Ben's KelloggCoin bal is " + getBalance("ben"));
 console.log("Jeff's KelloggCoin bal is " + getBalance("jeff"));
-console.log(blockchain.length)
